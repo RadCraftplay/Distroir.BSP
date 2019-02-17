e27cb317e446659db1d7fd785f47e1a00708b284
Distroir <radcraftplay2@gmail.com>
2019-02-17 20:45:55 +0100

    BspReader instances in unit tests will be now disposed

--- Unit tests/BspReaderTests.cs
+++ Unit tests/BspReaderTests.cs
@@ -16,18 +16,21 @@ namespace UnitTests
         public void CtorReadFile()
         {
             BspReader r = new BspReader(MAP_FILENAME);
+            r.Dispose();
         }
 
         [TestMethod]
         public void CtorReadFromFileStream()
         {
             BspReader r = new BspReader(new FileStream(MAP_FILENAME, FileMode.Open));
+            r.Dispose();
         }
 
         [TestMethod]
         public void CtorReadFromStream()
         {
             BspReader r = new BspReader((Stream)(new FileStream(MAP_FILENAME, FileMode.Open)));
+            r.Dispose();
         }
 
         [TestMethod]
@@ -35,6 +38,7 @@ namespace UnitTests
         {
             BspReader r = new BspReader(MAP_FILENAME);
             var info = r.ReadInfo();
+            r.Dispose();
 
             Assert.IsInstanceOfType(info, typeof(BspInfo));
             Assert.IsNotNull(info);
@@ -45,6 +49,7 @@ namespace UnitTests
         {
             BspReader r = new BspReader(MAP_FILENAME);
             var lump = r.ReadLumpInfo(BspLumps.LUMP_PAKFILE);
+            r.Dispose();
 
             Assert.IsInstanceOfType(lump, typeof(BspLump));
             Assert.IsNotNull(lump);
@@ -55,6 +60,7 @@ namespace UnitTests
         {
             BspReader r = new BspReader(MAP_FILENAME);
             var lump = r.ReadLumpInfo(40);
+            r.Dispose();
 
             Assert.IsInstanceOfType(lump, typeof(BspLump));
             Assert.IsNotNull(lump);
@@ -65,6 +71,7 @@ namespace UnitTests
         {
             BspReader r = new BspReader(MAP_FILENAME);
             var data = r.ReadLumpData(BspLumps.LUMP_PAKFILE);
+            r.Dispose();
 
             Assert.IsInstanceOfType(data, typeof(byte[]));
             Assert.IsNotNull(data);
@@ -75,6 +82,7 @@ namespace UnitTests
         {
             BspReader r = new BspReader(MAP_FILENAME);
             var data = r.ReadLumpData(40);
+            r.Dispose();
 
             Assert.IsInstanceOfType(data, typeof(byte[]));
             Assert.IsNotNull(data);
