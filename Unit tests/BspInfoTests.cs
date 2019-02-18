65f327e9660184ebbfd8b523d67f66162d172b19
Distroir <radcraftplay2@gmail.com>
2019-02-18 14:59:38 +0100

    Added unit tests for BspInfo class

---
+++ Unit tests/BspInfoTests.cs
@@ -0,0 +1,28 @@
+﻿using Distroir.Bsp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BspInfoTests
+    {
+        const string MAP_FILENAME = "testmap.bsp";
+
+        [TestMethod]
+        public void ComparisonTest()
+        {
+            BspReader r = new BspReader(MAP_FILENAME);
+            var info1 = r.ReadInfo();
+            r.Dispose();
+
+            r = new BspReader(MAP_FILENAME);
+            var info2 = r.ReadInfo();
+            r.Dispose();
+
+            Assert.AreEqual(info1, info2);
+        }
+    }
+}
