3f065c4f98959ecd40e34a440e81fb80a6457517
Distroir <radcraftplay2@gmail.com>
2019-02-18 14:55:25 +0100

    Added unit tests for BspLump class

---
+++ Unit tests/BspLumpTests.cs
@@ -0,0 +1,32 @@
+﻿using Distroir.Bsp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BspLumpTests
+    {
+        [TestMethod]
+        public void ComparisonTest()
+        {
+            var lump1 = new BspLump()
+            {
+                Version = 1,
+                FileLength = 2155,
+                FileOffset = 309
+            };
+
+            var lump2 = new BspLump()
+            {
+                Version = 1,
+                FileLength = 2155,
+                FileOffset = 309
+            };
+
+            Assert.AreEqual(lump1, lump2);
+        }
+    }
+}
