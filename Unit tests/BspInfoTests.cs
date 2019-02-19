ece4da8454dbc9817fb17967cdf5dad4047bc36a
Distroir <radcraftplay2@gmail.com>
2019-02-19 14:55:06 +0100

    Added 3 new tests for BspInfo class

--- Unit tests/BspInfoTests.cs
+++ Unit tests/BspInfoTests.cs
@@ -24,5 +24,62 @@ namespace UnitTests
 
             Assert.AreEqual(info1, info2);
         }
+
+        [TestMethod]
+        public void DifferentComparison()
+        {
+            BspInfo info = new BspInfo();
+            info.Version = 1;
+            info.Identifier = 1;
+            info.Lumps = new BspLump[1]
+            {
+                new BspLump()
+                {
+                    Version = 1,
+                    FileLength = 1,
+                    FileOffset = 1,
+                    fourCC = 1
+                }
+            };
+            info.MapRevision = 1;
+
+            BspInfo info2 = new BspInfo();
+            info2.Version = 1;
+            info2.Identifier = 1;
+            info2.Lumps = new BspLump[1]
+            {
+                new BspLump()
+                {
+                    Version = 1,
+                    FileLength = 1,
+                    FileOffset = 1,
+                    fourCC = 1
+                }
+            };
+            info2.MapRevision = 1;
+
+            Assert.AreEqual(info, info2);
+
+            info2.MapRevision = 37;
+            Assert.AreNotEqual(info, info2);
+        }
+
+        [TestMethod]
+        public void OneNullComparison()
+        {
+            BspReader r = new BspReader(MAP_FILENAME);
+            var info1 = r.ReadInfo();
+            r.Dispose();
+
+            Assert.AreNotEqual(info1, null);
+        }
+
+        [TestMethod]
+        public void BothNullComparison()
+        {
+            BspInfo info1 = null;
+
+            Assert.AreEqual(info1, null);
+        }
     }
 }
